626ef80aed1f098feee8428a06233871908e952e
ahmadalashour <ahmadalashour1998@gmail.com>
2023-12-22 22:02:19 +0100

    fishier .sh et .desktop pour linux users

--- Execute.sh
+++ Execute.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 # Set the path to the lib directory
-LIB_PATH=lib
+LIB_PATH="lib"
 
 # Set the path to the bin directory
-BIN_PATH=bin
+BIN_PATH="bin"
 
 # Compile the Java code with UTF-8 encoding
 javac --module-path "$LIB_PATH" --add-modules javafx.controls,javafx.fxml -encoding UTF-8 -d "$BIN_PATH" "App.java"
 
 # Run the compiled Java program with UTF-8 encoding
 java --module-path "$LIB_PATH" --add-modules javafx.controls,javafx.fxml -Dfile.encoding=UTF-8 -cp "$BIN_PATH" App
+
+# Keep the terminal open after execution (for debugging purposes)
+read -p "Press Enter to exit"
