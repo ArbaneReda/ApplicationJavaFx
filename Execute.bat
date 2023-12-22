bd1821cb584bca5499c812e042f7095f257f4a7c
ArbaneReda <arbane.reda1@gmail.com>
2023-12-22 17:10:15 +0100

    * Commande .bat

---
+++ Execute.bat
@@ -0,0 +1,3 @@
+@echo off
+cd /d "C:\Users\33778\Desktop\javafxprog\javafx-sdk-17.0.9"
+"C:\Users\33778\AppData\Local\Programs\Eclipse Adoptium\jdk-17.0.8.101-hotspot\bin\java.exe" @"C:\Users\33778\AppData\Local\Temp\cp_32j3zhskrdgkebaj75nuv77t3.argfile" app
