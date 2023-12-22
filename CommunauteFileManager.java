594b04455586f49df56cfa06a8afbbbe51f0ce6b
ArbaneReda <arbane.reda1@gmail.com>
2023-12-22 17:47:25 +0100

    *Lire fichier

--- CommunauteFileManager.java
+++ CommunauteFileManager.java
@@ -35,18 +35,24 @@ public class CommunauteFileManager {
         try {
             BufferedReader reader = new BufferedReader(new FileReader(file));
             String line;
-    
+
             while ((line = reader.readLine()) != null) {
+                // Pour les villes
                 if (line.startsWith("ville(")) {
-                    // Extraire le nom de la ville de la ligne (par exemple, "ville(A).")
-                    String villeName = line.substring(6, 7); // Suppose que le nom de la ville est un seul caractère
+                    // Supposons que le nom de la ville est tout ce qui se trouve entre les
+                    // parenthèses
+                    String villeName = line.substring(line.indexOf('(') + 1, line.indexOf(')'));
                     communaute.ajouterVille(new Ville(villeName));
-                } else if (line.startsWith("route(")) {
+                }
+
+                // Pour les routes
+                else if (line.startsWith("route(")) {
                     // Extraire les noms des villes liées par la route (par exemple, "route(A,B).")
-                    String[] parts = line.substring(6, line.length() - 2).split(",");
+                    String routeContent = line.substring(line.indexOf('(') + 1, line.indexOf(')'));
+                    String[] parts = routeContent.split(",");
                     if (parts.length == 2) {
-                        String ville1Name = parts[0];
-                        String ville2Name = parts[1];
+                        String ville1Name = parts[0].trim();
+                        String ville2Name = parts[1].trim();
                         Ville ville1 = communaute.getVille(ville1Name);
                         Ville ville2 = communaute.getVille(ville2Name);
                         if (ville1 != null && ville2 != null) {
@@ -54,16 +60,18 @@ public class CommunauteFileManager {
                             routesFromFile.add(new Pair<>(ville1Name, ville2Name));
                         }
                     }
-                } else if (line.startsWith("recharge(")) {
-                    // Extraire le nom de la ville avec une zone de recharge (par exemple, "recharge(B).")
-                    String villeName = line.substring(9, 10); // Suppose que le nom de la ville est un seul caractère
+                }
+
+                // Pour les zones de recharge
+                else if (line.startsWith("recharge(")) {
+                    String villeName = line.substring(line.indexOf('(') + 1, line.indexOf(')'));
                     Ville ville = communaute.getVille(villeName);
                     if (ville != null) {
                         ville.setZoneRecharge(true);
                     }
                 }
             }
-    
+
             reader.close();
         } catch (IOException e) {
             e.printStackTrace();
